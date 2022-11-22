61a10d9beb0b0c89d7cc1618ecae145eae7d6899
Biki Mahamood <92981044+biki404@users.noreply.github.com>
2022-11-22 16:48:59 +0530

    Update Biki.py

--- Biki.py
+++ Biki.py
@@ -7,7 +7,8 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from libbiki import main
+    from libbiki import login
     main()
 elif bit == '32bit':
-    print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
+    from libbiki import login
+    login()
