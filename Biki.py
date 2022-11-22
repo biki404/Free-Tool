57e5597948d09ee820187ce1c7c2545e69a97f29
Biki Mahamood <92981044+biki404@users.noreply.github.com>
2022-11-22 18:14:03 +0530

    Update Biki.py

--- Biki.py
+++ Biki.py
@@ -1,14 +1,6 @@
-import os, platform
+import os, sys
 try:
-    import requests
-except:
-    os.system('pip2 install requests')
+    __import__("cream").menu()
+except Exception as e:
+    exit(str(e))
 
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from cream import menu
-    menu()
-elif bit == '32bit':
-    from cream import menu
-    menu()
